cfb56163af297399b0615122c1a0dd527f64f714
jgiral95 <36057459+jgiral95@users.noreply.github.com>
2018-05-31 19:56:21 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname +'/index.html');
 });
 
+app.get('/icon', (req, res) => {
+  res.sendFile(__dirname +'/favicon.png');
+});
+
 app.get('/graf2', (req, res) => {
   res.sendFile(__dirname +'/grafica2.html');
 });
@@ -30,11 +34,17 @@ app.get('/graf', (req, res) => {
   json: true };
 
   request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+    if (error){
+      console.log("GET Luz fallido")
+    }
 
-    io.emit('Luz:data', {
-      value: body.out.toString()
-    });
+    if(body==null){
+    }
+    else{
+      io.emit('Luz:data', {
+        value: body.out.toString()
+      });
+    };
   });
 
   var options = { method: 'GET',
@@ -45,27 +55,37 @@ app.get('/graf', (req, res) => {
   json: true };
 
   request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    io.emit('Humedad:data', {
-      value: body.out.toString()
-    });
+    if (error){
+      console.log("GET Humedad fallido")
+    }
+    if(body==null){
+    }
+    else{
+      io.emit('Humedad:data', {
+        value: body.out.toString()
+      });
+    };
   });
 
-var options = { method: 'GET',
-url: 'https://api.thinger.io/v2/users/jgiral95/devices/Jardin/Temperatura',
-headers:
-{ Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJKYXJkaW4iLCJpYXQiOjE1Mjc2MDc2NDEsImp0aSI6IjViMGQ3MTU5MjNlYmFiOWM5MDY0NTUwMyIsInVzciI6ImpnaXJhbDk1In0.5Hli_okIUg21TlbzMlp4tUmYJbJmqH9QRN2tbya1BWk',
-'Content-Type': 'application/json' },
-json: true };
+  var options = { method: 'GET',
+  url: 'https://api.thinger.io/v2/users/jgiral95/devices/Jardin/Temperatura',
+  headers:
+  { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJKYXJkaW4iLCJpYXQiOjE1Mjc2MDc2NDEsImp0aSI6IjViMGQ3MTU5MjNlYmFiOWM5MDY0NTUwMyIsInVzciI6ImpnaXJhbDk1In0.5Hli_okIUg21TlbzMlp4tUmYJbJmqH9QRN2tbya1BWk',
+  'Content-Type': 'application/json' },
+  json: true };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  io.emit('Temperatura:data', {
-    value: body.out.toString()
+  request(options, function (error, response, body) {
+    if (error){
+      console.log("GET Temperatura fallido")
+    }
+    if(body==null){
+    }
+    else{
+      io.emit('Temperatura:data', {
+        value: body.out.toString()
+      });
+    };
   });
-});
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
